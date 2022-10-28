61fe04cecac3a7914fb592ed4c4c4b4c59d344e0
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2022-10-28 16:13:33 +0200

    created a connection to the MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes/notesRoutes");
+
+const connectDB = require("./db/connectDB");
+require("dotenv").config();
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+
 app.use("/api/v1/notes", routes);
 
 const port = 3000;
 
-app.listen(port, console.log(`server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
