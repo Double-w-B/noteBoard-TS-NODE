59ffa263ed21863bdc4929712cd80a8d96ad0921
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2022-11-24 23:21:33 +0100

    installed security packages

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+// security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 // routers
 const authRouter = require("./routes/authRoutes");
 const notesRouter = require("./routes/notesRoutes");
@@ -17,12 +23,21 @@ const notFound = require("./middleware/notFound");
 const errorHandlerMiddleware = require("./middleware/errorHandler");
 
 app.use(express.static("./public"));
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
-/* routes */
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/notes", authenticateUser, notesRouter);
-/* Custom error message */
+
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
